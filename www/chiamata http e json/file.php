05fd74f9c2661084dcfb05bcc69a10df5b6627f0
FRAKORN1 <FRAKORN1@GMAIL.COM>
2014-12-19 11:03:55 +0100

    second commit

---
+++ www/chiamata http e json/file.php
@@ -0,0 +1,87 @@
+[
+{
+"Name" : "Alfreds Futterkiste",
+"City" : "Berlin",
+"Country" : "Germany"
+},
+{
+"Name" : "Berglunds snabbkp",
+"City" : "Lulea",
+"Country" : "Sweden"
+},
+{
+"Name" : "Centro comercial Moctezuma",
+"City" : "Mexico D.F.",
+"Country" : "Mexico"
+},
+{
+"Name" : "Ernst Handel",
+"City" : "Graz",
+"Country" : "Austria"
+},
+{
+"Name" : "FISSA Fabrica Inter. Salchichas S.A.",
+"City" : "Madrid",
+"Country" : "Spain"
+},
+{
+"Name" : "Galeria del gastronomo",
+"City" : "Barcelona",
+"Country" : "Spain"
+},
+{
+"Name" : "Island Trading",
+"City" : "Cowes",
+"Country" : "UK"
+},
+{
+"Name" : "Koniglich Essen",
+"City" : "Brandenburg",
+"Country" : "Germany"
+},
+{
+"Name" : "Laughing Bacchus Wine Cellars",
+"City" : "Vancouver",
+"Country" : "Canada"
+},
+{
+"Name" : "Magazzini Alimentari Riuniti",
+"City" : "Bergamo",
+"Country" : "Italy"
+},
+{
+"Name" : "North/South",
+"City" : "London",
+"Country" : "UK"
+},
+{
+"Name" : "Paris specialites",
+"City" : "Paris",
+"Country" : "France"
+},
+{
+"Name" : "Rattlesnake Canyon Grocery",
+"City" : "Albuquerque",
+"Country" : "USA"
+},
+{
+"Name" : "Simons bistro",
+"City" : "Kobenhavn",
+"Country" : "Denmark"
+},
+{
+"Name" : "The Big Cheese",
+"City" : "Portland",
+"Country" : "USA"
+},
+{
+"Name" : "Vaffeljernet",
+"City" : "arhus",
+"Country" : "Denmark"
+},
+{
+"Name" : "Wolski Zajazd",
+"City" : "Warszawa",
+"Country" : "Poland"
+}
+]
